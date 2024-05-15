718d80fde78749a88f549485b14239c21870751e
Franck Gildas <efmukamk01@monccnb.ca>
2024-05-15 00:19:02 -0300

    Modified the src icons images path

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,26 +1,26 @@
 export const sidebarLinks = [
   {
-    imgURL: "/src/assets/icons/home.svg",
+    imgURL: "/assets/icons/home.svg",
     route: "/",
     label: "Accueil",
   },
   {
-    imgURL: "/src/assets/icons/wallpaper.svg",
+    imgURL: "/assets/icons/wallpaper.svg",
     route: "/explore",
     label: "Explorer",
   },
   {
-    imgURL: "/src/assets/icons/people.svg",
+    imgURL: "/assets/icons/people.svg",
     route: "/all-users",
     label: "Utilisateurs",
   },
   {
-    imgURL: "/src/assets/icons/bookmark.svg",
+    imgURL: "/assets/icons/bookmark.svg",
     route: "/saved",
     label: "Enregistré",
   },
   {
-    imgURL: "/src/assets/icons/gallery-add.svg",
+    imgURL: "/assets/icons/gallery-add.svg",
     route: "/create-post",
     label: "Créer publication",
   },
@@ -28,22 +28,22 @@ export const sidebarLinks = [
 
 export const bottombarLinks = [
   {
-    imgURL: "/src/assets/icons/home.svg",
+    imgURL: "/assets/icons/home.svg",
     route: "/",
     label: "Home",
   },
   {
-    imgURL: "/src/assets/icons/wallpaper.svg",
+    imgURL: "/assets/icons/wallpaper.svg",
     route: "/explore",
     label: "Explore",
   },
   {
-    imgURL: "/src/assets/icons/bookmark.svg",
+    imgURL: "/assets/icons/bookmark.svg",
     route: "/saved",
     label: "Saved",
   },
   {
-    imgURL: "/src/assets/icons/gallery-add.svg",
+    imgURL: "/assets/icons/gallery-add.svg",
     route: "/create-post",
     label: "Create",
   },
